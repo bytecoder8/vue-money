4012b213d77b1b312e38d49f6db0a33a4e44fc1c
bytecoder8 <58306822+bytecoder8@users.noreply.github.com>
2019-12-12 22:35:06 +0500

    Add date filter and dropdown

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -5,7 +5,7 @@
         <a href="#" @click.prevent="$emit('menu-click')">
           <i class="material-icons black-text">dehaze</i>
         </a>
-        <span class="black-text">12.12.12</span>
+        <span class="black-text">{{date | date('datetime')}}</span>
       </div>
 
       <ul class="right hide-on-small-and-down">
@@ -14,6 +14,7 @@
               class="dropdown-trigger black-text"
               href="#"
               data-target="dropdown"
+              ref="dropdown"
           >
             USER NAME
             <i class="material-icons right">arrow_drop_down</i>
@@ -21,13 +22,13 @@
 
           <ul id='dropdown' class='dropdown-content'>
             <li>
-              <a href="#" class="black-text">
+              <router-link to="/profile" class="black-text">
                 <i class="material-icons">account_circle</i>Профиль
-              </a>
+              </router-link>
             </li>
             <li class="divider" tabindex="-1"></li>
             <li>
-              <a href="#" class="black-text">
+              <a href="#" class="black-text" @click.prevent="logout">
                 <i class="material-icons">assignment_return</i>Выйти
               </a>
             </li>
@@ -40,6 +41,32 @@
 
 <script>
 export default {
-  
+  data() {
+    return {
+      date: new Date(),
+      dropdown: null,
+      interval: null
+    }
+  },
+  mounted() {
+    this.dropdown = window.M.Dropdown.init(this.$refs.dropdown, {
+
+    })
+
+    this.interval = setInterval(() => {
+      this.date = new Date()
+    }, 1000)
+  },
+  beforeDestroy() {
+    clearInterval(this.interval)
+    if (this.dropdown && this.dropdown.destroy) {
+      this.dropdown.destroy()
+    }
+  },
+  methods: {
+    logout() {
+      this.$router.push('/login?message=logout')
+    }
+  }
 }
 </script>
